d5b166413990f9b3d7cc31ab7c07f9e7eb50b43d
Thiago Jampaulo Xavier <xavierThiago@users.noreply.github.com>
2018-06-16 11:31:50 -0300

    Margem à direita no ícone de enviar
    Margem à direita no ícone de enviar para o terapeuta

--- js/record.js
+++ js/record.js
@@ -35,7 +35,7 @@ const createAudio = ((audio) => {
   $(`<li data-audio-url="${audio.audioUrl}" data-audio-id="${gravacoes.length - 1}" class="collection-item avatar">
   <i class="material-icons circle blue">play_arrow</i><span></span>
       <p><span class="title">${gravacoes.length}) Treino</span>
-      <a href="#" data-send-audio class="secondary-content"><i class="material-icons ico-darkred">send</i>
+      <a href="#" data-send-audio class="secondary-content" style="margin-right:2em;"><i class="material-icons ico-darkred">send</i>
       <a href="#!" data-remove-audio class="secondary-content"><i class="material-icons ico-darkred">remove_circle_outline</i>
       </a>
       </p>
